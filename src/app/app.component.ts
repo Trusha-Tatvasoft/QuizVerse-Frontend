f66b4b955fc78501172b7e2488f54e3d53f0c2ec
BhumiTatva <tatva.pce91@outlook.com>
2025-07-09 18:59:12 +0530

    [fun]: reusable confirmation dialoge craeted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PageHeaderComponent } from './shared/components/page-header/page-header.component';
+import { ConfirmationDialogData } from './shared/interfaces/constants.static';
+import { ConfirmationDialogComponent } from './shared/components/confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,24 @@ import { PageHeaderComponent } from './shared/components/page-header/page-header
 })
 export class AppComponent {
   title = 'QuizVerse-Frontend';
+
+  constructor(private readonly dialog: MatDialog) {}
+
+  openDeleteDialog() {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        title: 'Delete Category',
+        message:
+          'Are you sure you want to delete this category? This action cannot be undone and will affect all quizzes in this category.',
+        confirmText: 'Delete',
+        cancelText: 'Cancel',
+      } as ConfirmationDialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // Perform deletion logic
+      }
+    });
+  }
 }
---
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmationDialogData } from '../../interfaces/constants.static';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  imports: [MatDialogModule, MatButtonModule, CommonModule],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.scss',
+})
+export class ConfirmationDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData,
+  ) {}
+
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
+}
