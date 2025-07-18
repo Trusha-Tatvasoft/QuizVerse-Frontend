b1370cb1027bfdef5d88ba75037cc809273e9797
Darsh-Tatvasoft <tatva.pca195@outlook.com>
2025-07-18 18:51:44 +0530

    removal of unnecessary imports from app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginSignupComponent } from './core/auth/components/login-signup/login-signup.component';
-import { LoginComponent } from './core/auth/components/login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginSignupComponent, LoginComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
