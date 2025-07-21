c7e3f7afffe07d7ad9ead1cd7b782b363cea1f54
Darsh-Tatvasoft <tatva.pca195@outlook.com>
2025-07-21 16:18:35 +0530

    Authentication page (#21)
    * layout of auth-main component and signin component
    
    * login/signup page complete
    
    * removal of unnecessary imports from app
    
    * comments changes
    
    * removed absolute paths
    
    * added validator email pattern and corrected the format of the ts files
    
    * applied proper validators patterns for each fields as instructued

---
+++ src/app/core/auth/components/login-signup/login-signup.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { OutlineButtonComponent } from '../../../../shared/components/outline-button/outline-button.component';
+import {
+  FACEBOOK_BUTTON_CONFIG,
+  GOOGLE_BUTTON_CONFIG,
+  LOGIN_SIGNUP_TABS_CONFIG,
+} from '../../configs/login-signup.component.config';
+import { TabComponent } from '../../../../shared/components/tab/tab.component';
+
+/**
+ * Login/Signup container component
+ * - Displays Google and Facebook login buttons
+ * - Displays tab layout for Sign In / Sign Up
+ */
+@Component({
+  selector: 'app-login-signup',
+  imports: [OutlineButtonComponent, TabComponent],
+  templateUrl: './login-signup.component.html',
+  styleUrl: './login-signup.component.scss',
+})
+export class LoginSignupComponent {
+  googleButton = GOOGLE_BUTTON_CONFIG; // Google button configuration
+  facebookButton = FACEBOOK_BUTTON_CONFIG; // Facebook button configuration
+  tabs = LOGIN_SIGNUP_TABS_CONFIG; // Tabs for Sign In / Sign Up
+  selectedIndex = 0; // Active tab index
+}
---
+++ src/app/core/auth/interfaces/register.interface.ts
@@ -0,0 +1,8 @@
+/**
+ * Interface for register credentials
+ */
+export interface RegisterCredentials {
+  fullName: string;
+  email: string;
+  password: string;
+}
