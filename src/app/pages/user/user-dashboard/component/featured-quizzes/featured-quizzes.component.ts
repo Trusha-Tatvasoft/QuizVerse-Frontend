739c6c99cb017aa4b239dc15b6918565b9dfec62
vivekkotadiya17 <tatva.pci135@outlook.com>
2025-08-27 09:16:13 +0530

    to create folder structure for user-dashboard

---
+++ src/app/pages/user/user-dashboard/component/featured-quizzes/featured-quizzes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-quizzes',
+  imports: [],
+  templateUrl: './featured-quizzes.component.html',
+  styleUrl: './featured-quizzes.component.scss',
+})
+export class FeaturedQuizzesComponent {}
