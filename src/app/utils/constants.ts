11ccaca557c160685a9a62f49a72c306d5c879be
vivekkotadiya17 <tatva.pci135@outlook.com>
2025-09-12 14:59:20 +0530

    to fix bug in user-dashboard and admin-dashboard

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -546,3 +546,20 @@ export const questionDifficultyMessages = {
   deleteQuestionDifficulty: 'Question difficulty deleted successfully.',
 };
 // #endregion
+
+// #region admin dashboard graph colors
+export const adminDashboardChartColors = {
+  // Bar chart
+  bar: {
+    border: '#4caf50',
+    fill: '#a6f1a6',
+  },
+
+  // Doughnut chart
+  doughnut: {
+    border: '#7c3aed',
+    fill: ['#ede9fe', '#ddd6fe', '#e0e7ff', '#f5f3ff'],
+  },
+};
+
+// #endregion
