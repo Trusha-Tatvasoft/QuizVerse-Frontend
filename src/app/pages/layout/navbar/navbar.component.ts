61ac8e19edaddccb0a75223573d2b600dd3c3b39
BhumiTatva <tatva.pce91@outlook.com>
2025-09-24 16:39:17 +0530

    Notification center module and navbar section of notification,other data get with API call

--- src/app/pages/layout/navbar/navbar.component.ts
+++ src/app/pages/layout/navbar/navbar.component.ts
@@ -5,6 +5,8 @@ import {
   HostListener,
   inject,
   Input,
+  OnDestroy,
+  OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
@@ -17,6 +19,7 @@ import { TextButtonComponent } from '../../../shared/components/text-button/text
 import { FilledButtonComponent } from '../../../shared/components/filled-button/filled-button.component';
 import {
   deleteButtonConfig,
+  getNotificationRoute,
   getStartedButtonConfig,
   markAsAllReadButtonConfig,
   markAsReadButtonConfig,
@@ -31,10 +34,15 @@ import { OutlineButtonComponent } from '../../../shared/components/outline-butto
 import { Notifications } from '../interfaces/navbar.component.interface';
 import { Router } from '@angular/router';
 import { Navigations } from '../../../shared/enums/navigation';
-import { plateformName, roles } from '../../../utils/constants';
+import { defaultLogoPath, plateformName, platformMessages, roles } from '../../../utils/constants';
 import { AuthService } from '../../../core/auth/services/auth.service';
 import { PlatformSettingsService } from '../../../services/admin/platform-settings/platform-settings.service';
+import { NavbarDataService } from '../../../services/common/navbar/navbar-data.service';
 import { Subject, takeUntil } from 'rxjs';
+import { SnackbarService } from '../../../shared/service/snackbar/snackbar.service';
+import { environment } from '../../../../environments/environment.dev';
+import { TimeAgoPipe } from '../../../shared/pipes/time-ago/time-ago.pipe';
+import { UserProfileService } from '../../../services/user/user-profile/user-profile.service';
 
 @Component({
   selector: 'app-navbar',
@@ -49,18 +57,14 @@ import { Subject, takeUntil } from 'rxjs';
     TagComponent,
     ProgressBarComponent,
     OutlineButtonComponent,
+    TimeAgoPipe,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   @Input() isLogin = false;
   @Input() isAdmin = false;
-  @Input() currentXp = 0;
-  @Input() xpLimit = 1000;
-  @Input() profileImageUrl: string = 'assets/images/profile-1.png';
-  @Input() notificationCount = 99;
-  @Input() notifications: Notifications[] = [];
 
   @Output() openSidebar = new EventEmitter<void>();
   @Output() closeSidebar = new EventEmitter<void>();
@@ -81,13 +85,22 @@ export class NavbarComponent {
   showNotifications = false;
   menuOpen = false;
   logoPath: string | null;
+  currentXp = 0;
+  progressPercentage: number = 0;
+  profileImageUrl: string = 'assets/images/profile-1.png';
+  notificationCount = 99;
+  notifications: Notifications[] = [];
 
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
   private readonly platformSettingsService = inject(PlatformSettingsService);
+  private readonly navbarService = inject(NavbarDataService);
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  // private readonly notificationCenterService = inject(NotificationCenterService);
+  private readonly snackbarService = inject(SnackbarService);
+  private readonly userProfileService = inject(UserProfileService);
 
   private readonly destroy$ = new Subject<void>();
-
   private previousWidth = window.innerWidth;
 
   ngOnInit(): void {
@@ -97,6 +110,13 @@ export class NavbarComponent {
       this.role = role;
     });
     this.loadPlatformConfig();
+    this.loadUserData();
+
+    this.userProfileService.profileUpdated$.pipe(takeUntil(this.destroy$)).subscribe((updated) => {
+      if (updated) {
+        this.loadUserData();
+      }
+    });
   }
 
   @HostListener('window:resize', [])
@@ -120,6 +140,10 @@ export class NavbarComponent {
   toggleNotifications(event: Event) {
     event.stopPropagation(); // prevent immediate close
     this.showNotifications = !this.showNotifications;
+
+    if (this.showNotifications) {
+      this.loadNotifications();
+    }
   }
 
   browseQuizRedirect() {
@@ -146,7 +170,7 @@ export class NavbarComponent {
 
   goToProfile() {
     const route =
-      this.role === 'admin'
+      this.role === roles.admin
         ? `/${Navigations.Admin}/${Navigations.Profile}`
         : `/${Navigations.User}/${Navigations.Profile}`;
 
@@ -155,7 +179,7 @@ export class NavbarComponent {
 
   goToSetting() {
     const route =
-      this.role === 'admin'
+      this.role === roles.admin
         ? `/${Navigations.Admin}/${Navigations.Profile}/`
         : `/${Navigations.User}/${Navigations.Profile}`;
 
@@ -172,9 +196,7 @@ export class NavbarComponent {
       return;
     }
 
-    const role = this.authService.currentRole$.value?.toLowerCase() || '';
-
-    switch (role) {
+    switch (this.role) {
       case roles.admin:
         this.router.navigate([`/${Navigations.Admin}/${Navigations.Dashboard}`]);
         break;
@@ -189,6 +211,98 @@ export class NavbarComponent {
 
   imageError() {
     this.logoPath = null;
+  }
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  markAsRead(id: string) {
+    // this.notificationCenterService.markAsRead(id).pipe(takeUntil(this.destroy$)).subscribe({
+    //   next: (res) => {
+    //     if (res.result) {
+    //       this.snackbarService.showError(
+    //         platformMessages.successTitle,
+    //         res.message
+    //       );
+    //       this.loadNotifications();
+    //     }
+    //     this.snackbarService.showError(
+    //       platformMessages.errorTitle,
+    //       res.message || platformMessages.errorMessage,
+    //     );
+    //   },
+    //   error: (err) => {
+    //     this.snackbarService.showError(
+    //       platformMessages.errorTitle,
+    //       err?.error?.message || platformMessages.errorMessage,
+    //     );
+    //   },
+    // });
+  }
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  deleteNotification(id: string) {
+    // this.notificationCenterService.deleteNotification(id).pipe(takeUntil(this.destroy$)).subscribe({
+    //   next: (res) => {
+    //     if (res.result) {
+    //       this.loadNotifications();
+    //       this.snackbarService.showSuccess(
+    //         platformMessages.successTitle,
+    //         res.message
+    //       )
+    //     }
+    //     this.snackbarService.showError(
+    //       platformMessages.errorTitle,
+    //       res.message || platformMessages.errorMessage,
+    //     );
+    //   },
+    //   error: (err) => {
+    //     this.snackbarService.showError(
+    //       platformMessages.errorTitle,
+    //       err?.error?.message || platformMessages.errorMessage,
+    //     );
+    //   },
+    // });
+  }
+
+  viewAllNotifications() {
+    this.showNotifications = false;
+    const route =
+      this.role === roles.admin
+        ? `/${Navigations.Admin}/${Navigations.Notifications}/`
+        : `/${Navigations.User}/${Navigations.Notifications}`;
+    this.router.navigate([route]);
+  }
+
+  viewDetails(category: number) {
+    const { path, queryParams } = getNotificationRoute(this.role === roles.admin, category);
+    this.router.navigate([path], { queryParams });
+  }
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  markAsAllRead() {
+    // this.notificationCenterService
+    //   .markAllAsRead()
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe({
+    //     next: (res) => {
+    //       if (res.result) {
+    //         this.snackbarService.showSuccess(
+    //           platformMessages.successTitle,
+    //           res.message
+    //         );
+    //         this.loadNotifications();
+    //       }
+    //       this.snackbarService.showError(
+    //         platformMessages.errorTitle,
+    //         res.message || platformMessages.errorMessage,
+    //       );
+    //     },
+    //     error: (err) => {
+    //       this.snackbarService.showError(
+    //         platformMessages.errorTitle,
+    //         err?.error?.message || platformMessages.errorMessage,
+    //       );
+    //     },
+    //   });
   }
 
   ngOnDestroy(): void {
@@ -211,5 +325,65 @@ export class NavbarComponent {
           this.logoPath = config.logo ?? null;
         }
       });
+  }
+
+  private loadUserData() {
+    this.navbarService
+      .getNavbarData()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (res) => {
+          if (res.result) {
+            this.currentXp = res.data.currentUserXp ?? 0;
+            this.progressPercentage = res.data.progressPercentage ?? 0;
+            this.notificationCount = res.data.notificationCount;
+            this.profileImageUrl = res.data.profilePic
+              ? `${environment.imageBaseUrl}/${res.data.profilePic}`
+              : 'assets/images/profile-1.png';
+          }
+        },
+        error: (err) => {
+          this.snackbarService.showError(
+            platformMessages.errorTitle,
+            err?.error?.message || platformMessages.errorMessage,
+          );
+        },
+      });
+  }
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  private loadNotifications(): void {
+    // const payload: NotificationCenterRequest = {
+    //   searchText: '',
+    //   adminCategory: 0,
+    //   userCategory: 0,
+    //   type: 0,
+    //   time: 0,
+    //   isAdmin: this.role === roles.admin,
+    //   notificationStatusSelected: 0
+    // }; // Remove after Backend done
+    // this.notificationCenterService
+    //   .getNotifications(payload)
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe({
+    //     next: (res) => {
+    //       if (res.result && res.data) {
+    //         this.notifications = res.data.notifications.map((notif) =>
+    //           mapNotification(notif, this.isAdmin)
+    //         );
+    //       } else {
+    //         this.snackbarService.showError(
+    //           platformMessages.errorTitle,
+    //           res.message || platformMessages.errorMessage,
+    //         );
+    //       }
+    //     },
+    //     error: (err) => {
+    //       this.snackbarService.showError(
+    //         platformMessages.errorTitle,
+    //         err?.error?.message || platformMessages.errorMessage,
+    //       );
+    //     },
+    //   });
   }
 }
---
+++ src/app/pages/layout/notification-center/notification-center.component.ts
@@ -0,0 +1,211 @@
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { PageHeaderComponent } from '../../../shared/components/page-header/page-header.component';
+import {
+  buildNotificationTabs,
+  notificationHeaderConfig,
+  searchInputConfig,
+  timeFilters,
+} from '../configs/notification-center.component.config';
+import { SearchInputComponent } from '../../../shared/components/search-input/search-input.component';
+import { FormControl } from '@angular/forms';
+import { TabComponent } from '../../../shared/components/tab/tab.component';
+import { markAsAllReadButtonConfig } from '../configs/navbar.component.config';
+import { OutlineButtonComponent } from '../../../shared/components/outline-button/outline-button.component';
+import {
+  clearFilterButtonConfig,
+  showFilterButtonConfig,
+} from '../../user/browse-quizzes/configs/browse-quizzes.config';
+import {
+  AdminNotificationCategory,
+  NotifcationTabName,
+  UserNotificationCategory,
+} from '../../../shared/enums/notification-center.enum';
+import { MatFormField, MatSelectModule } from '@angular/material/select';
+import { FilledButtonComponent } from '../../../shared/components/filled-button/filled-button.component';
+import { enumToCategoryList, typeList } from './notification-center.comonent.mapper';
+import { AuthService } from '../../../core/auth/services/auth.service';
+import { roles } from '../../../utils/constants';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
+import { Notifications } from '../interfaces/navbar.component.interface';
+
+@Component({
+  selector: 'app-notification-center',
+  imports: [
+    PageHeaderComponent,
+    SearchInputComponent,
+    TabComponent,
+    OutlineButtonComponent,
+    MatSelectModule,
+    MatFormField,
+    FilledButtonComponent,
+  ],
+  templateUrl: './notification-center.component.html',
+  styleUrls: [
+    './notification-center.component.scss',
+    '../../user/browse-quizzes/browse-quizzes.component.scss',
+  ],
+})
+export class NotificationCenterComponent implements OnInit, OnDestroy {
+  notificationConfig = notificationHeaderConfig;
+  searchInputConfig = searchInputConfig;
+  markAsAllReadButton = markAsAllReadButtonConfig;
+  showFilterBtn = showFilterButtonConfig;
+  clearFilterBtn = clearFilterButtonConfig;
+
+  // State
+  selectedTab = 0;
+  selectedAdminCategory: number;
+  selectedUserCategory: number;
+  selectedType: number;
+  selectedTime: number;
+  notifications = signal<Notifications[]>([]);
+  searchControl = new FormControl<string | null>(null);
+  isAdmin = false;
+
+  // Filters
+  typeList = typeList();
+  adminCategoryList = enumToCategoryList(AdminNotificationCategory);
+  userCategoryList = enumToCategoryList(UserNotificationCategory);
+  timeFilters = timeFilters;
+
+  // Counts per tab
+  tabCounts = signal<Record<NotifcationTabName, number>>({
+    [NotifcationTabName.All]: 0,
+    [NotifcationTabName.Unread]: 0,
+    [NotifcationTabName.Read]: 0,
+    [NotifcationTabName.Urgent]: 0,
+  });
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  // private readonly service = inject(NotificationCenterService);
+  // private readonly snackbarService = inject(SnackbarService);
+  private readonly authService = inject(AuthService);
+  private readonly searchSubject$ = new Subject<string>();
+  private readonly destroy$ = new Subject<void>();
+
+  // Tabs builder
+  get tabs() {
+    return buildNotificationTabs(this.tabCounts());
+  }
+
+  ngOnInit(): void {
+    const token = this.authService.getAccessToken();
+    if (token) {
+      const role = this.authService.getRoleFromToken(token);
+      if (role === roles.admin) {
+        this.isAdmin = true;
+      }
+    }
+
+    // search listener
+    this.searchSubject$
+      .pipe(debounceTime(400), distinctUntilChanged(), takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.fetchNotifications();
+      });
+
+    this.fetchNotifications();
+  }
+
+  onSearchInputChange(value: string): void {
+    this.searchSubject$.next(value);
+  }
+
+  onTabChanged(index: number) {
+    this.selectedTab = index;
+    this.fetchNotifications();
+  }
+
+  onFilterChange() {
+    this.fetchNotifications();
+  }
+
+  clearFilters() {
+    this.searchControl.setValue(null);
+    this.selectedAdminCategory = 0;
+    this.selectedTime = 0;
+    this.selectedType = 0;
+    this.selectedUserCategory = 0;
+
+    this.fetchNotifications();
+  }
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  markAsAllRead() {
+    // this.service
+    //   .markAllAsRead()
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe({
+    //     next: (res) => {
+    //       if (res.result) {
+    //         this.snackbarService.showSuccess(
+    //           platformMessages.successTitle,
+    //           res.message
+    //         );
+    //         this.fetchNotifications();
+    //       }
+    //       this.snackbarService.showError(
+    //         platformMessages.errorTitle,
+    //         res.message || platformMessages.errorMessage,
+    //       );
+    //     },
+    //     error: (err) => {
+    //       this.snackbarService.showError(
+    //         platformMessages.errorTitle,
+    //         err?.error?.message || platformMessages.errorMessage,
+    //       );
+    //     },
+    //   });
+  }
+
+  //NOTE: Uncomment and use after backend API is implemented to fetch and display notifications
+  fetchNotifications(): void {
+    // const enumValues = Object.values(NotifcationTabName).filter(v => typeof v === 'number') as number[];
+    // const selectedEnum = enumValues[this.selectedTab];
+    // const payload: NotificationCenterRequest = {
+    //   searchText: this.searchControl.value ?? '',
+    //   adminCategory: this.isAdmin ? this.selectedAdminCategory : 0,
+    //   userCategory: !this.isAdmin ? this.selectedUserCategory : 0,
+    //   type: this.selectedType ?? 0,
+    //   time: this.selectedTime ?? 0,
+    //   isAdmin: this.isAdmin,// Remove after Backend done
+    //   notificationStatusSelected: selectedEnum
+    // };
+    // this.service
+    //   .getNotifications(payload)
+    //   .pipe(takeUntil(this.destroy$))
+    //   .subscribe({
+    //     next: (res) => {
+    //       if (res.result && res.data) {
+    //         // map backend to UI cards
+    //         this.notifications.set(
+    //           res.data.notifications.map((notif) => mapNotification(notif, this.isAdmin))
+    //         );
+    //         // update tab counts
+    //         this.tabCounts.set({
+    //           [NotifcationTabName.All]: res.data.all ?? 0,
+    //           [NotifcationTabName.Unread]: res.data.unread ?? 0,
+    //           [NotifcationTabName.Read]: res.data.read ?? 0,
+    //           [NotifcationTabName.Urgent]: res.data.urgent ?? 0,
+    //         });
+    //       } else {
+    //         this.snackbarService.showError(
+    //           platformMessages.errorTitle,
+    //           res.message || platformMessages.errorMessage,
+    //         );
+    //       }
+    //     },
+    //     error: (err) => {
+    //       this.snackbarService.showError(
+    //         platformMessages.errorTitle,
+    //         err?.error?.message || platformMessages.errorMessage,
+    //       );
+    //     },
+    //   });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
