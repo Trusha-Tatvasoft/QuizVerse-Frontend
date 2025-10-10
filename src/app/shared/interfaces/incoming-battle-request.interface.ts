73de7193956278d242496501ba4dab70508ee4b7
vivekkotadiya17 <tatva.pci135@outlook.com>
2025-10-10 11:34:26 +0530

    to remove comment

--- src/app/shared/interfaces/incoming-battle-request.interface.ts
+++ src/app/shared/interfaces/incoming-battle-request.interface.ts
@@ -1,12 +1,11 @@
 export interface IncomingBattleRequest {
-  requestId: number; // Unique identifier for the battle request
+  requestId: number;
   senderUserName: string;
   senderFullName: string;
   senderProfilePic: string;
-  battleName: string; // Name of the user who sent the request
-  battleCategory: string; // Category of the battle (e.g., Math, Coding)
-  battleDifficulty: string; // Difficulty level
-  // You can add optional fields if needed
-  sendingDate?: Date; // Timestamp when the request was created
-  timeAgo?: string; // Optional avatar image URL
+  battleName: string;
+  battleCategory: string;
+  battleDifficulty: string;
+  sendingDate?: Date;
+  timeAgo?: string;
 }
